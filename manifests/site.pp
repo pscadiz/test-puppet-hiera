dbd3de36140536e51e573cb853100d6fd480dd80
Paolo Cadiz <pcadiz@cambridge.org>
2017-06-01 12:07:15 +0800

    updated site manifest

---
+++ manifests/site.pp
@@ -0,0 +1,19 @@
+node 'pao-puppetmaster.buildandrelease.org' {
+	class { 'ntp':
+		servers => [ 'ntp1.corp.com', 'ntp2.corp.com' ],
+	}
+}
+
+node 'pao-puppetslave.buildandrelease.org' {
+	include ntp
+	class { 'profile::release_accounts': 
+		ensure => 'absent',
+	}
+	include ::role::webserver
+}
+
+node 'pao-wordpress-1.buildandrelease.org' {
+	include role::amp
+	include profile::wordpress
+}
+
